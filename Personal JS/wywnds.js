7550dc3274615dbe44658cbab54292527d450244
Hvftk <57172166+Hvftk@users.noreply.github.com>
2019-11-12 12:58:02 +0800

    Add files via upload

---
+++ Personal JS/wywnds.js
@@ -0,0 +1,11 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.tradeEndTime = 1879685290000;
+body = JSON.stringify(obj);
+$done(body);
+
+/**
+ * @supported 7EE45021521D
+ */
+//By yxiaocai & JO2EY
