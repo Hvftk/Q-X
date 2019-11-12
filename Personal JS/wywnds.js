c6b833647faf1dda54e4bee31eaf35f8f38e419c
Hvftk <57172166+Hvftk@users.noreply.github.com>
2019-11-12 13:11:41 +0800

    Update wywnds.js

--- Personal JS/wywnds.js
+++ Personal JS/wywnds.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var body = $response.body;
 var obj = JSON.parse(body);
 
@@ -10,16 +9,3 @@ $done(body);
  * @supported 7EE45021521D
  */
 //By yxiaocai & JO2EY
-=======
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj.tradeEndTime = 1879685290000;
-body = JSON.stringify(obj);
-$done(body);
-
-/**
- * @supported 7EE45021521D
- */
-//By yxiaocai & JO2EY
->>>>>>> 7550dc3274615dbe44658cbab54292527d450244
