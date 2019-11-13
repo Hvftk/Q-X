7112065d4f30f3bda1a054f4a664f9ee15a21307
Hvftk <57172166+Hvftk@users.noreply.github.com>
2019-11-13 16:41:06 +0800

    update

--- Personal JS/Wechat.js
+++ Personal JS/Wechat.js
@@ -1,15 +1,22 @@
 /*
-[rewrite_local]
-^https?://mp.weixin.qq.com/mp/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+Remove the WeChat public account bottom ad
 
-hostname = mp.weixin.qq.com,
+QX:
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+Surge4：
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+Surge & QX MITM = mp.weixin.qq.com,
 */
 
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
-$done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(obj)}); 
 
 /**
  * @supported 7EE45021521D
  */ 
+ 
+//by Choler
