06c2b484db04b8f54169a79e55a3432a1e00e064
Hvftk <57172166+Hvftk@users.noreply.github.com>
2019-11-14 11:05:48 +0800

    add Over

---
+++ Personal JS/Over.js
@@ -0,0 +1,9 @@
+var body = $response.body;
+let obj = JSON.parse(body);
+obj.user.subscription["isSubscriptionActive"] = true;
+obj.user.subscription["entitlement"] = ["OVER_PRO"];
+$done({body:JSON.stringify(obj)});
+
+/**
+ * @supported 7EE45021521D
+ */
